f5e64faf530e4864db30a92daa6292749a9e348b
Annu-Ag <89718115+Annu-Ag@users.noreply.github.com>
2023-05-18 10:29:14 +0530

    Add files via upload
    Added few new component

--- eonboarding/ClientApp/src/app/company-culture/company-culture.component.ts
+++ eonboarding/ClientApp/src/app/company-culture/company-culture.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-company-culture',
@@ -8,14 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-culture.component.css']
 })
 export class CompanyCultureComponent {
-
   carouselImages = [
-    { src: '../', alt: 'Image 1' },
-    { src: '../../assets/images/moback-bg2.jpg', alt: 'Image 2' },
-    { src: '../../assets/images/moback.jpeg', alt: 'Image 3' },
-    // Add more images as needed
+    { 
+      heading: 'Moback Offsite 2022', 
+      details: 'Moback Offsite was a great event orgainised by Moback team for all the employees.Everyone from different part of India joined in person and took part in the event.It was full of fun with team-bondind activities, dance, play and laugh.' ,
+      images: [
+        { src: '../../assets/images/offsite-1.jpeg', alt: 'Image 1' },
+        { src: '../../assets/images/offsite-2.jpeg', alt: 'Image 2' },
+        { src: '../../assets/images/offsite-3.jpeg', alt: 'Image 3' },
+        { src: '../../assets/images/offsite-4.jpg', alt: 'Image 4' }
+      ]
+    },
+    { 
+      heading: 'Moback Diwali Celebration 2022', 
+      details: 'Some glimse of Deepavali celebration in office premises',
+      images: [
+        { src: '../../assets/images/diwali-3.jpeg', alt: 'Image 5' },
+        { src: '../../assets/images/diwali-2.jpeg', alt: 'Image 6' }
+      ]
+    },
+    { 
+      heading: 'Moback CSR Program – Free Health Check Up Camp', 
+      details: '',
+      images: [
+        { src: '../../assets/images/CSR-2.jpeg', alt: 'Image 7' },
+        { src: '../../assets/images/CSR-1.jpeg', alt: 'Image 8' },
+      ]
+    },
   ];
-
-
 }
-
--- eonboarding/ClientApp/src/app/offer-letter-generation/offer-letter-generation.component.ts
+++ eonboarding/ClientApp/src/app/offer-letter-generation/offer-letter-generation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-offer-letter-generation',
@@ -10,11 +11,32 @@ export class OfferLetterGenerationComponent {
   position: string = '';
   offerDetails: string = '';
 
-  generateOfferLetter() {
-    // Logic to generate the offer letter using the entered details
-    console.log('Offer letter generated for:', this.candidateName);
-    console.log('Position:', this.position);
-    console.log('Offer details:', this.offerDetails);
+  generateOfferLetter(): void {
+    // Logic to generate the offer letter based on candidate details
+    const offerLetter = `
+      Date: ${new Date().toLocaleDateString()}
+      
+      Dear ${this.candidateName},
+      
+      We are pleased to offer you the position of ${this.position} at our company. We have carefully reviewed your qualifications and believe that you will be a valuable asset to our team.
+      
+      Offer Details:
+      
+      ${this.offerDetails}
+      
+      Please review the offer terms and conditions carefully. If you have any questions or concerns, please don't hesitate to contact us.
+      
+      We look forward to welcoming you to our organization!
+      
+      Sincerely,
+      Moback Technologies Pvt Ltd
+    `;
+  
+    // Create a new Blob with the offer letter content
+    const blob = new Blob([offerLetter], { type: 'text/plain;charset=utf-8' });
+  
+    // Save the offer letter file using FileSaver.js
+    saveAs(blob, 'offer-letter.txt');
   }
 }
 
