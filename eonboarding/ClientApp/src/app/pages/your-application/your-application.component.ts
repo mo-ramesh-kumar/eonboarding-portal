fc2ddc01b5eca19fc16f0a0b058dc3adb6529426
kf-ramesh-kumar <ramesh.kumar@kornferry.com>
2023-05-23 23:08:28 +0530

    Fixed issues

--- eonboarding/ClientApp/src/app/pages/your-application/your-application.component.ts
+++ eonboarding/ClientApp/src/app/pages/your-application/your-application.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./your-application.component.css']
 })
 export class YourApplicationComponent implements OnInit {
-
+  candidateDetails: any = {
+    name: '',
+    dob: '',
+    nationality: '',
+    gender: '',
+    email: '',
+    phone: '',
+    school: '',
+    schoolPercentage: null,
+    college: '',
+    collegePercentage: null,
+    company: '',
+    technology: '',
+    skills: '',
+    postCollege: '',
+    postCollegePercentage: ''
+  };
   constructor() { }
   candidate = {
     editingProfile: true,
@@ -14,6 +30,30 @@ export class YourApplicationComponent implements OnInit {
     trainings: false
   };
   ngOnInit(): void {
+    this.populateFieldsFromResume(null);
   }
 
+  populateFieldsFromResume(resumeData: any) {
+    // Simulated data population (example only, not a complete solution)
+    // Replace this code with your resume parsing logic
+    this.candidateDetails.name = 'Megha Chawdhary';
+    this.candidateDetails.dob = '1990-05-15';
+    this.candidateDetails.nationality = 'Indian';
+    this.candidateDetails.gender = 'female';
+    this.candidateDetails.email = 'megha.chawdhary@mail.com';
+    this.candidateDetails.phone = '8668699664';
+    this.candidateDetails.school = 'ABC School';
+    this.candidateDetails.schoolPercentage = 80;
+    this.candidateDetails.college = 'XYZ College';
+    this.candidateDetails.collegePercentage = 85;
+    this.candidateDetails.company = 'XYZ Inc.';
+    this.candidateDetails.technology = 'Angular, React, Node.js';
+    this.candidateDetails.skills = 'JavaScript, HTML, CSS';
+    this.candidateDetails.postCollege = 'Sastra University';
+    this.candidateDetails.postCollegePercentage = 80;
+  }
+
+  handleSave(){
+    
+  }
 }
