a5ce1ff3a426ab63b5e995f1ebe1851cd2669d20
kf-ramesh-kumar <ramesh.kumar@kornferry.com>
2023-05-26 21:21:01 +0530

    Fixed side nav

--- eonboarding.Models/Candidate.cs
+++ eonboarding.Models/Candidate.cs
@@ -2,9 +2,9 @@
 {
     public class Candidate
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
     }
 }
--- eonboarding/ClientApp/src/app/shared/sidebar/sidebar.component.ts
+++ eonboarding/ClientApp/src/app/shared/sidebar/sidebar.component.ts
@@ -9,12 +9,248 @@ import { AuthService } from '../../services/auth.service';
 export class SidebarComponent implements OnInit {
   loginType: string = '';
 
+  candidateNavItems = [
+    {
+      title: 'Home',
+      url: '/home',
+      isSelected: false,
+      css: 'fas fa-home',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Your Application',
+      url: '/your-application',
+      isSelected: false,
+      css: 'fas fa-file-edit',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Document Center',
+      url: 'Document Center',
+      isSelected: false,
+      css: 'fas fa-chevron-down',
+      order: 0,
+      subItems: [
+        {
+          title: 'Onboarding-documents',
+          url: '/onboarding-documents',
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        },
+        {
+          title: 'generate-digital-signature',
+          url: '/generate-digital-signature',
+          isSelected: false,
+          css: 'fas fa-tasks',
+          order: 0,
+        },
+        {
+          title: 'Employee NDA',
+          url: '/assets/documents/Employee NDA .pdf',
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        },
+        {
+          title: 'Leave Policy and Holiday',
+          url: '/assets/documents/Moback IDC - Leave, Working Hours and Holiday Policy - 13th April 2021.pdf',
+          isLink: true,
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        },
+        {
+          title: 'Employee Handbook',
+          url: '/assets/documents/Moback-IDC-Employee-HandBook-R2.0.pdf',
+          isLink: true,
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        }
+      ]
+    },
+    {
+      title: 'trainings',
+      url: '/trainings',
+      isSelected: false,
+      css: 'fas fa-briefcase',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'benefits',
+      url: '/benefits',
+      isSelected: false,
+      css: 'fas fa-tasks',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'team-members">',
+      url: '/team-members">',
+      isSelected: false,
+      css: 'fas fa-tasks',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'about-us">',
+      url: '/about-us">',
+      isSelected: false,
+      css: 'fas fa-home',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Company Culture">',
+      url: '/company-culture">',
+      isSelected: false,
+      css: 'fas fa-globe',
+      order: 0,
+      subItems: [
+       
+      ]
+    }
+  ]
+
+  adminNavItems = [
+    {
+      title: 'Dashboard',
+      url: '/dashboard',
+      isSelected: true,
+      css: 'fas fa-home',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Profile Management',
+      url: '/profile-management',
+      isSelected: false,
+      css: 'fas fa-user-cog',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Onboarding Checklist',
+      url: '/onboarding-checklist',
+      isSelected: false,
+      css: 'fas fa-list-check',
+      order: 0,
+      subItems: [
+       
+      ]
+    },
+    {
+      title: 'Document Center',
+      url: '/document-Center',
+      isSelected: false,
+      css: 'fas fa-file-alt',
+      order: 0,
+      subItems: [
+        {
+          title: 'Faculty Contract Agreement',
+          url: '/assets/documents/Faculty Contract Agreement.pdf',
+          isLink: true,
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        },
+        {
+          title: 'Code of Conduct',
+          url: '/assets/documents/Code of Conduct.pdf',
+          isLink: true,
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        },
+        {
+          title: 'Policies and Procedures Manual',
+          url: '/assets/documents/Policies and Procedures Manual.pdf',
+          isLink: true,
+          isSelected: false,
+          css: 'fas fa-file-pdf',
+          order: 0,
+        }
+      ]
+    }, {
+      title: 'Admin Center',
+      url: '/admin-Center',
+      isSelected: false,
+      css: 'fas fa-user-cog',
+      order: 0,
+      subItems: [
+        {
+          title: 'Candidate Enrollment',
+          url: '/candidate-enrollment',
+          isSelected: false,
+          isLink: false,
+          css: 'fas fa-user-cog',
+          order: 0,
+        },
+        {
+          title: 'Document review',
+          url: '/document-review',
+          isSelected: false,
+          isLink: false,
+          css: 'fas fa-clipboard-check',
+          order: 0,
+        },
+        {
+          title: 'Offer letter generation',
+          url: '/offer-letter-generation',
+          isSelected: false,
+          isLink: false,
+          css: 'fas fa-file-signature',
+          order: 0,
+        },
+        {
+          title: 'Admin Approvals',
+          url: '/admin-approvals',
+          isSelected: false,
+          isLink: false,
+          css: 'fas fa-thumbs-up',
+          order: 0,
+        }
+      ]
+    }    
+  ]
+
   constructor(private AuthService: AuthService) {
 
   }
 
-  ngOnInit(): void {   
+  ngOnInit(): void {
     this.loginType = this.AuthService.getLoginType();
   }
 
+  getIcon(item: any){
+    return item.css;
+  }
+
+  setNavSelection(item: any){
+
+    this.adminNavItems.map(item => item.isSelected = false);
+    this.candidateNavItems.map(item => item.isSelected = false);
+
+    item.isSelected = true;
+  }
 }
